5f0d982f059be0bd3d0b95f255c22159503d3069
cytcool <675149948@qq.com>
2018-01-26 17:29:38 +0800

    数据结构和日期工具类

--- app/src/main/java/com/example/cyt/accountingapp/DateUtil.java
+++ app/src/main/java/com/example/cyt/accountingapp/DateUtil.java
@@ -1,8 +1,23 @@
 package com.example.cyt.accountingapp;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by CYT on 2018/1/26.
  */
 
 public class DateUtil {
+
+    public static String getFormattedTime(long timeStamp){
+
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
+        return formatter.format(new Date(timeStamp));
+
+    }
+
+    public static String getFormattedDate(){
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        return formatter.format(new Date());
+    }
 }
--- app/src/main/java/com/example/cyt/accountingapp/RecordBean.java
+++ app/src/main/java/com/example/cyt/accountingapp/RecordBean.java
@@ -23,5 +23,65 @@ public class RecordBean {
 
     public RecordBean(){
         uuid = UUID.randomUUID().toString();
+        timeStamp = System.currentTimeMillis();
+        date = DateUtil.getFormattedDate();
     }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public RecordType getType() {
+        return type;
+    }
+
+    public void setType(RecordType type) {
+        this.type = type;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+
 }
