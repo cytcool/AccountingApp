8066e1007a31a7b897332571e27f9ae874684e6b
cytcool <675149948@qq.com>
2018-01-28 22:30:58 +0800

    主界面头部bug修复

--- app/src/main/java/com/example/cyt/accountingapp/MainActivity.java
+++ app/src/main/java/com/example/cyt/accountingapp/MainActivity.java
@@ -5,10 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    private Button btnCreate;
-    private RecordDataBaseHelper dbHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,14 +15,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         getSupportActionBar().setElevation(0);
 
-        dbHelper = new RecordDataBaseHelper(this,"Record",null,1);
 
-        btnCreate = findViewById(R.id.btn_create);
-        btnCreate.setOnClickListener(this);
+
     }
 
-    @Override
-    public void onClick(View v) {
-        dbHelper.getWritableDatabase();
-    }
+
 }
