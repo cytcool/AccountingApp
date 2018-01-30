41067769071bfb11cb3e6d2d86068d7e21da0af3
cytcool <675149948@qq.com>
2018-01-30 22:01:15 +0800

    账目添加界面键盘按键逻辑编写

--- app/src/main/java/com/example/cyt/accountingapp/AddRecordActivity.java
+++ app/src/main/java/com/example/cyt/accountingapp/AddRecordActivity.java
@@ -2,9 +2,11 @@ package com.example.cyt.accountingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
-public class AddRecordActivity extends AppCompatActivity {
+public class AddRecordActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button kb_one;
     private Button kb_two;
@@ -14,7 +16,13 @@ public class AddRecordActivity extends AppCompatActivity {
     private Button kb_six;
     private Button kb_seven;
     private Button kb_eight;
-    private Button kb_night;
+    private Button kb_nine;
+    private Button kb_zero;
+
+    private Button kb_dot;
+    private ImageButton kb_type;
+    private ImageButton kb_backspace;
+    private ImageButton kb_done;
 
 
     @Override
@@ -35,6 +43,73 @@ public class AddRecordActivity extends AppCompatActivity {
         kb_five = findViewById(R.id.keyboard_five);
         kb_six= findViewById(R.id.keyboard_six);
         kb_seven = findViewById(R.id.keyboard_seven);
+        kb_eight= findViewById(R.id.keyboard_eight);
+        kb_nine = findViewById(R.id.keyboard_nine);
+        kb_zero = findViewById(R.id.keyboard_zero);
+
+        kb_one.setOnClickListener(this);
+        kb_two.setOnClickListener(this);
+        kb_three.setOnClickListener(this);
+        kb_four.setOnClickListener(this);
+        kb_five.setOnClickListener(this);
+        kb_six.setOnClickListener(this);
+        kb_seven.setOnClickListener(this);
+        kb_eight.setOnClickListener(this);
+        kb_nine.setOnClickListener(this);
+        kb_zero.setOnClickListener(this);
+
+        handleBackSpace();
+        handleDone();
+        handleDot();
+        handleTypeChange();
+
    }
+
+   private void handleDot(){
+        kb_dot = findViewById(R.id.keyboard_dot);
+        kb_dot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+   }
+
+   private void handleTypeChange(){
+       kb_type = findViewById(R.id.keyboard_type);
+       kb_type.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+
+           }
+       });
+   }
+
+   private void handleBackSpace(){
+       kb_backspace = findViewById(R.id.keyboard_backspace);
+       kb_backspace.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+
+           }
+       });
+   }
+
+   private void handleDone(){
+       kb_done = findViewById(R.id.keyboard_done);
+       kb_done.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+
+           }
+       });
+   }
+
+    @Override
+    public void onClick(View v) {
+        Button button = (Button) v;
+        String input = button.getText().toString();
+
+    }
 }
 
